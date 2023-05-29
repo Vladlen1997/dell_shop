b67c0f70076d0be96fcf38228883426d9d4f94ec
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-29 15:57:39 +0300

    first commit

---
+++ local/templates/dell_index/description.php
@@ -0,0 +1,12 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die(); ?>
+
+
+
+<?php
+
+$arTemplate = [
+"NAME" => 'Шаблон главной страницы',
+ "DESCRIPTION" => "описание шаблона"
+];
+
+?>
